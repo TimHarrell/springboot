6b608b707329f7f206bb9825a1f13c08128a1c93
TimHarrell <mrmath1618@gmail.com>
2018-09-26 16:39:07 -0500

    simple HTTP responses working

---
+++ src/main/java/springboot/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // tells spring framework that this class will handle HTTP requests with HTTP responses
+public class HelloController {
+
+	@RequestMapping("/hello") // tells spring framework that this method should be run when the url is accessed
+	public String sayHi() {
+		return "hi"; // just returns simple text
+	}
+}
+
+//notes
+	// there is an issue with sending the HTTP request when the controller and the main method are not in the same package/subpackage
